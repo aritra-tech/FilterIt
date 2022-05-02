3f7f4290db3897d0c958feb74ad2596c1b11bfc0
Aritra Das <aritrarick2002@gmail.com>
2022-05-03 02:06:33 +0530

    Image Picker Added

--- app/src/main/java/com/geekymusketeers/imagefilter/EditImageScreen.kt
+++ app/src/main/java/com/geekymusketeers/imagefilter/EditImageScreen.kt
@@ -1,11 +1,36 @@
 package com.geekymusketeers.imagefilter
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.geekymusketeers.imagefilter.databinding.ActivityEditImageScreenBinding
 
 class EditImageScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditImageScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_image_screen)
+        binding = ActivityEditImageScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListerner()
+        displayImagePreview()
+    }
+
+    private fun displayImagePreview() {
+        intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.imagePreview.setImageBitmap(bitmap)
+            binding.imagePreview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setListerner() {
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/geekymusketeers/imagefilter/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/imagefilter/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.geekymusketeers.imagefilter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.geekymusketeers.imagefilter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setListerners()
+    }
+
+    private fun setListerners(){
+        binding.editNewImage.setOnClickListener {
+            Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            ).also { pickerIntent ->
+                pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK){
+            data?.data?.let { imageUri ->
+                Intent(applicationContext, EditImageScreen::class.java).also { editImageIntent ->
+                    editImageIntent.putExtra(KEY_IMAGE_URI,imageUri)
+                    startActivity(editImageIntent)
+                }
+            }
+        }
     }
 }
